f5c0cd655081140c6705479600633f0f10325d8d
wanghong <89240220@qq.com>
2013-06-21 14:16:33 +0800

    创建新的ios项目，命名为newMaimaicha

---
+++ newMaimaicha/newMaimaicha/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  newMaimaicha
+//
+//  Created by ken on 13-6-21.
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ newMaimaicha/newMaimaicha/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  newMaimaicha
+//
+//  Created by ken on 13-6-21.
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
